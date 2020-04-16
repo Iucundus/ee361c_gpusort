03e6e5ee295ffbdf917b9bd0ef6d52bd78c2c67d
John Koelling <john.koelling@utexas.edu>
2020-04-16 15:38:44 -0500

    Brick Sort
    Implemented brick sort

--- brick_sort.cu
+++ brick_sort.cu
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include "file_helper.h"
 
+__global__ void brickSort(int* array, int arrayLen, int p) {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx >= arrayLen - 1)
+		return;
+	if ((p % 2 == 0) && (idx % 2 == 1))
+		return;
+	if ((p % 2 == 1) && (idx % 2 == 0))
+		return;
+	if (array[idx] > array[idx + 1]) {
+		int tmp = array[idx + 1];
+		array[idx + 1] = array[idx];
+		array[idx] = tmp;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	//Set default file name
 	const char* inputFile = "inp.txt";
@@ -21,6 +36,21 @@ int main(int argc, char* argv[]) {
 	}
 	
 	//YOUR CODE HERE
+	int* d_array;
+	cudaMalloc(&d_array, size * sizeof(int));
+	cudaMemcpy(d_array, A, size * sizeof(int), cudaMemcpyHostToDevice);
+
+	// To get the first element, first pass number is 0
+	// Last pass number is probably (size - 2)
+	// By zero-indexing, that means (size - 1) passes
+	// If the largest element is in array[0], that gives (size - 1) passes to move it (size - 1) places to array[size - 1]
+	for (int i = 0; i < size - 1; i++) {
+		brickSort<<<(size + 1023)/1024, 1024>>>(d_array, size, i);
+		cudaDeviceSynchronize();
+	}
+
+	cudaMemcpy(A, d_array, size * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaFree(d_array);
 	
 	//Output array to file
 	if(saveArray(outputFile, A, size) < 0) {
