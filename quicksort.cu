d587c6a3b22d6830bb434ec6c2f8634975536884
Kaboomtastic <james.java.sullivan@gmail.com>
2020-04-24 18:30:34 -0500

    fixed compile errors

--- quicksort.cu
+++ quicksort.cu
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "file_helper.h"
 
-__global__ void split(int N, int* input, int* left, int* right);
+__global__ void split(int N, int* input, int* left, int* right, int* leftcount, int* rightcount);
 
 struct node{
 	int* array;
@@ -10,6 +10,8 @@ struct node{
 	node* left;
 	node* right;
 };
+void recursive_helper(node* current_node);
+int* quick_sort(int N, int* input);
 
 int main(int argc, char* argv[]) {
 	//Set default file name
@@ -30,14 +32,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	//YOUR CODE HERE
-	int* input;
-	int* left;
-	int* right;
-
-	cudaMallocManaged(&left, size*sizeof(int));
-	cudaMallocManaged(&right, size*sizeof(int));
-	cudaMallocManaged(&input, size*sizeof(int));
-
+	A = quick_sort(size,A);
 
 
 	//Output array to file
@@ -54,25 +49,27 @@ int main(int argc, char* argv[]) {
 
 
 __global__
-void split(int N, int* input, int* left, int* right, int* leftcount int* rightcount){
+void split(int N, int* input, int* left, int* right, int* leftcount, int* rightcount){
   int index = threadIdx.x;
   int stride = blockDim.x;
 
   __shared__ int local_left_count;
   __shared__ int local_right_count;
-
+  local_left_count = 0;
+  local_right_count = 0;
+  __syncthreads();
   int splitter = input[0];
 
   for (int i = index+1; i < N; i+= stride){
     if(input[i] > splitter){
-      right[atomicAdd(&rightcount,1)] = input[i];
+      right[atomicAdd(&local_right_count,1)] = input[i];
     }else{
-      left[atomicAdd(&leftcount,1)] = input[i];
+      left[atomicAdd(&local_left_count,1)] = input[i];
     }
   }
   __syncthreads();
   if(index == 0){
-    left[atomicAdd(&leftcount,1)] = input[0];
+    left[atomicAdd(&local_left_count,1)] = input[0];
 		*leftcount = local_left_count;
 		*rightcount = local_right_count;
   }
@@ -86,28 +83,33 @@ int* quick_sort(int N, int* input){
 	node root;
 	root.array = input;
 	root.numElements = N;
-	output = malloc(sizeof(int)*N);
+	output = (int*)malloc(sizeof(int)*N);
 	output_count = 0;
 	recursive_helper(&root);
+	return output;
 }
 
-void recursive_helper(node* current_node){
-
+void recursive_helper(node* the_node){
+	node current_node = *the_node;
+	printf("%d ",current_node.numElements);
 	if(current_node.numElements < 2){
 		output[output_count] = current_node.array[0];
 		output_count++;
 		return;
 	}
-	current_node.left = malloc(sizeof(node));
-	current_node.right = malloc(sizeof(node));
-
-	cudaMallocManaged(&current_node.left.array, sizeof(int)*current_node.numElements);
-	cudaMallocManaged(&current_node.right.array, sizeof(int)*current_node.numElements);
+	current_node.left = (node*)malloc(sizeof(node));
+	current_node.right = (node*)malloc(sizeof(node));
+	node left_node = *current_node.left;
+	node right_node = *current_node.right;
+	cudaMallocManaged(&left_node.array, sizeof(int)*current_node.numElements);
+	cudaMallocManaged(&right_node.array, sizeof(int)*current_node.numElements);
 	//TODO add break condition
 	
 
-	split<<<1,1>>>(size, current_node.array, current_node.left.array, current_node.right.array, &current_node.left.numElements, &current_node.right.numElements);
-
+	split<<<1,1>>>(current_node.numElements, current_node.array, left_node.array, right_node.array, &left_node.numElements, &right_node.numElements);
+	cudaDeviceSynchronize();
+	printf("%d left",left_node.numElements);
+	printf("%d right",right_node.numElements);
 	recursive_helper(current_node.left);
 	recursive_helper(current_node.right);
 
