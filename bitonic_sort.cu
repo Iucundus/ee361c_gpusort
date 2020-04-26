1a43d2a6ad444b0695f4f6fc90183762fa39bdad
Will Easterby <william.easterby@gmail.com>
2020-04-26 01:11:24 -0500

    Bitonic sort (locally)
    Working locally, not tested on maverick yet

--- bitonic_sort.cu
+++ bitonic_sort.cu
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 #include "file_helper.h"
 
+#define THREADS_PER_BLOCK 1024
+
+__global__ void bitonic_sort(int* arrayIn, int* arrayOut, int arrayLen, int chunkSize){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < arrayLen) {
+		int myValue = arrayIn[idx];
+		int chunkStart = (idx / chunkSize) * chunkSize;
+		int chunkMid = chunkStart + (chunkSize / 2);
+		int partnerIndex = chunkSize - (idx - chunkStart) - 1 + chunkStart;
+		if (partnerIndex < arrayLen) {
+			int partnerValue = arrayIn[partnerIndex];
+			int min = (myValue <= partnerValue) ? myValue:partnerValue;
+			int max = (myValue > partnerValue) ? myValue:partnerValue;
+			myValue = (idx < chunkMid) ? min:max;
+		}
+		arrayOut[idx] = myValue;
+	}
+}
+
+__global__ void bitonic_merge(int* arrayIn, int* arrayOut, int arrayLen, int chunkSize){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < arrayLen) {
+		int myValue = arrayIn[idx];
+		int chunkStart = (idx / chunkSize) * chunkSize;
+		int chunkMid = chunkStart + (chunkSize / 2);
+		int partnerIndex = (((idx - chunkStart) + (chunkSize / 2)) % chunkSize) + chunkStart;
+		if (partnerIndex < arrayLen) {
+			int partnerValue = arrayIn[partnerIndex];
+			int min = (myValue <= partnerValue) ? myValue:partnerValue;
+			int max = (myValue > partnerValue) ? myValue:partnerValue;
+			myValue = (idx < chunkMid) ? min:max;
+		}
+		arrayOut[idx] = myValue;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	//Set default file name
 	const char* inputFile = "inp.txt";
@@ -21,6 +57,35 @@ int main(int argc, char* argv[]) {
 	}
 	
 	//YOUR CODE HERE
+	int *d_arrayIn, *d_arrayOut;
+	cudaMalloc(&d_arrayIn, size * sizeof(int));
+	cudaMalloc(&d_arrayOut, size * sizeof(int));
+	cudaMemcpy(d_arrayIn, A, size * sizeof(int), cudaMemcpyHostToDevice);
+
+	for(int sortSize = 2; (sortSize / 2) < size; sortSize = sortSize * 2) {
+		//Sort
+		bitonic_sort<<<(size + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_arrayIn, d_arrayOut, size, sortSize);
+		cudaDeviceSynchronize();
+			
+		int *d_temp = d_arrayOut;
+		d_arrayOut = d_arrayIn;
+		d_arrayIn = d_temp;
+		
+		
+		//Merge
+		for(int mergeSize = sortSize / 2; mergeSize > 1; mergeSize = mergeSize / 2){
+			bitonic_merge<<<(size + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_arrayIn, d_arrayOut, size, mergeSize);
+			cudaDeviceSynchronize();
+			
+			int *d_temp = d_arrayOut;
+			d_arrayOut = d_arrayIn;
+			d_arrayIn = d_temp;
+		}
+	}
+	
+	cudaMemcpy(A, d_arrayIn, size * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaFree(d_arrayIn);
+	cudaFree(d_arrayOut);
 	
 	//Output array to file
 	if(saveArray(outputFile, A, size) < 0) {
