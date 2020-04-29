28f7e917d10b439e6ac0a6c62342da73aed36bd8
John Koelling <jokoelli@login1.maverick2.tacc.utexas.edu>
2020-04-29 13:15:29 -0500

    Tweaks to testing script

---
+++ generic_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Argument: executable filename to run
+#Also produces scripttesttime
+
+echo -n > scripttesttime
+
+for x in {100000..1000000..100000} ; do
+	echo "Testing $1 for input size $x"
+	./generate_array $x test${x}.txt 1000
+	echo -n "${x} " >> scripttesttime
+	(time ./${1} test${x}.txt output${x}.txt) 2>&1 | grep sys >> scripttesttime
+	./validate test${x}.txt output${x}.txt
+	echo
+	echo
+done
