275ea68b8dd9289bfad4dbc79a1e4adf4e4ef1de
John Koelling <jokoelli@login1.maverick2.tacc.utexas.edu>
2020-04-30 13:01:34 -0500

    Added specific tests

--- generic_test.sh
+++ generic_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #Argument: executable filename to run
-#Also produces scripttesttime which lists the time taken for each array size
+#Also produces file $2 which lists the time taken for each array size
 
-echo -n > scripttesttime
+echo -n > $2
 
 for x in {100000..1000000..100000} ; do
 	echo "Testing $1 for input size $x"
 	./generate_array $x test${x}.txt 1000
-	echo -n "${x} " >> scripttesttime
-	(time ./${1} test${x}.txt output${x}.txt) 2>&1 | grep sys >> scripttesttime
+	echo -n "${x} " >> $2
+	(time ./${1} test${x}.txt output${x}.txt) 2>&1 | grep sys >> $2
 	./validate test${x}.txt output${x}.txt
 	echo
 	echo
